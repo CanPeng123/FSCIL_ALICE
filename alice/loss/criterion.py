26e342090240a41caf0c3a8b8b6270d45ccb0caa
Can Peng <46548883+CanPeng123@users.noreply.github.com>
2022-07-14 17:45:01 +1000

    Create criterion.py

---
+++ alice/loss/criterion.py
@@ -0,0 +1,51 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+class AngularPenaltySMLoss(nn.Module):
+    def __init__(self, loss_type='arcface', eps=1e-7, s=None, m=None):
+        '''
+        Angular Penalty Softmax Loss
+        Three 'loss_types' available: ['arcface', 'sphereface', 'cosface']
+        These losses are described in the following papers:
+
+        ArcFace: https://arxiv.org/abs/1801.07698
+        SphereFace: https://arxiv.org/abs/1704.08063
+        CosFace/Ad Margin: https://arxiv.org/abs/1801.05599
+        '''
+
+        super(AngularPenaltySMLoss, self).__init__()
+        loss_type = loss_type.lower()
+        assert loss_type in ['arcface', 'sphereface', 'cosface', 'crossentropy']
+        if loss_type == 'arcface':
+            self.s = 64.0 if not s else s
+            self.m = 0.5 if not m else m
+        if loss_type == 'sphereface':
+            self.s = 64.0 if not s else s
+            self.m = 1.35 if not m else m
+        if loss_type == 'cosface':
+            self.s = 30.0 if not s else s
+            self.m = 0.4 if not m else m
+        self.loss_type = loss_type
+        self.eps = eps
+
+        self.cross_entropy = nn.CrossEntropyLoss()
+
+    def forward(self, wf, labels):
+        if self.loss_type == 'crossentropy':
+            return self.cross_entropy(wf, labels)
+        else:
+            if self.loss_type == 'cosface':
+                numerator = self.s * (torch.diagonal(wf.transpose(0, 1)[labels]) - self.m)
+            if self.loss_type == 'arcface':
+                numerator = self.s * torch.cos(torch.acos(
+                    torch.clamp(torch.diagonal(wf.transpose(0, 1)[labels]), -1. + self.eps, 1 - self.eps)) + self.m)
+            if self.loss_type == 'sphereface':
+                numerator = self.s * torch.cos(self.m * torch.acos(
+                    torch.clamp(torch.diagonal(wf.transpose(0, 1)[labels]), -1. + self.eps, 1 - self.eps)))
+
+            excl = torch.cat([torch.cat((wf[i, :y], wf[i, y + 1:])).unsqueeze(0) for i, y in enumerate(labels)], dim=0)
+            denominator = torch.exp(numerator) + torch.sum(torch.exp(self.s * excl), dim=1)
+            L = numerator - torch.log(denominator)
+            return -torch.mean(L)
