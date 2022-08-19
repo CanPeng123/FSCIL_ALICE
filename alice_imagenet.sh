0904198aa1e66078dc660a4fc261c43fca2cc50d
Can Peng <46548883+CanPeng123@users.noreply.github.com>
2022-08-19 23:03:38 +1000

    Create alice_imagenet.sh

---
+++ alice_imagenet.sh
@@ -0,0 +1,22 @@
+python main_base.py \
+--dataset mini_imagenet \
+--data_root ./DATA \
+--exp_dir ./exp/MINI-IMAGENET_base-data-2-fusion_batch-64_epoch-200_lr-1e-2_milestones-60-70_seed-1_s-30_m-4e-1 \
+--arch resnet18 \
+--learning_rate 0.01 \
+--epochs_base 200 \
+--epochs_new 100 \
+--weight_decay 5e-4 \
+--momentum 0.9 \
+--milestones 60 70 \
+--gamma 0.1 \
+--batch_size 64 \
+--eval_freq 1 \
+--save_freq 25 \
+--feat_dim 2048 \
+--loss_type cosface \
+--loss_s 30 \
+--loss_m 0.4 \
+--seed 1  \
+--data_transform \
+--data_fusion
